5c0dbbe14ce9a6d5a6544815c19d71b332c12030
rahul62785 <53399538+rahul62785@users.noreply.github.com>
2019-07-30 10:21:06 +0530

    This program gives you square and cube of all numbers

---
+++ q3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int x;
+ int square;
+ int cube;
+
+ printf("\n Enter A Number: ");
+ scanf("%d",&x);
+
+ square = x*x;
+ cube = x*x*x;
+
+ printf(" Square  is = %d\n",square);
+
+ printf(" Cube  is = %d\n",cube);
+ return 0;
+}
