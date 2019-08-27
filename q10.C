e63890d24cc9576f7eb286a64078c80314c06f33
rahul62785 <53399538+rahul62785@users.noreply.github.com>
2019-08-27 21:00:23 +0530

    Write a Program to check the given number is Positive, Negative.

---
+++ q10.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+  clrscr();
+  printf("enter a value:");
+  scanf("%d",&a);
+  if(a>0)
+  {
+  printf("\n posetive");
+  }
+  else
+  {
+  printf("\n negetive");
+  }
+  getch();
+}
